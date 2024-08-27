8f521f658201996859050b5d6f47d88fa19b6636
jessica4263 <166820446+jessica4263@users.noreply.github.com>
2024-08-27 10:40:11 +0200

    Structure in App.js , import files in all files

---
+++ src/components/SongPlayer/SongPlayer.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import styles from './SongPlayer.module.css';
+
+function SongPlayer() {
+
+  return (
+    <div>
+        {/*Washitappe for the Album Picture*/}
+        <img/>
+        <img/>
+        <img/>
+        <img/>
+        {/*Album Picture*/}
+        <img></img>
+        {/*Play/Pause button*/}
+        <div>
+            <img/>
+            <img/>
+        </div>
+    </div>
+  );
+}
+
+export default SongPlayer;
