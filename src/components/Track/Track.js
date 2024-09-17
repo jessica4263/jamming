38b61274886bb2c07897b33fee5b0ee8cc22f332
jessica4263 <166820446+jessica4263@users.noreply.github.com>
2024-09-17 17:09:44 +0200

    feature: add-remove-btn

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,29 +1,17 @@
 import React, { useState } from 'react';
 import styles from './Track.module.css';
-import { ReactComponent as PlayIcon } from '../../assets/play-button.svg';
-import { ReactComponent as AddIcon } from '../../assets/add-button.svg';
 
-function Track({ name, artist, album}) {
+function Track({ name, artist, album }) {
 
   return (
     <>
-    <div>
-        {/*For every song that appears in the search*/}
-        <div className={styles.trackContainer}>
-          <div className={styles.font}>
-            <div className={styles.nameSong}>{name}</div>
-            <div className={styles.nameArtist}>{artist}</div>
-            <div className={styles.nameAlbum}>{album}</div>
-          </div>
-          {/*Buttons add and play*/}
-          <div className={styles.iconsContainer}>
-            <PlayIcon className={styles.icon} />
-            <AddIcon className={styles.icon} />
-          </div>
-        </div>
-        <hr/>
-    </div>
-  </>
+      {/*For every song that appears in the search*/}
+      <div className={styles.font}>
+        <div className={styles.nameSong}>{name}</div>
+        <div className={styles.nameArtist}>{artist}</div>
+        <div className={styles.nameAlbum}>{album}</div>
+      </div>
+    </>
   );
 }
 
--- test.js
+++ test.js
@@ -88,3 +88,34 @@ function App() {
 //     console.error("Error:", error);
 //   }
 // };
+
+// import React, { useState } from 'react';
+// import styles from './Track.module.css';
+// import { ReactComponent as PlayIcon } from '../../assets/play-button.svg';
+// import { ReactComponent as AddIcon } from '../../assets/add-button.svg';
+
+// function Track({ name, artist, album}) {
+
+//   return (
+//     <>
+//     <div>
+//         {/*For every song that appears in the search*/}
+//         <div className={styles.trackContainer}>
+//           <div className={styles.font}>
+//             <div className={styles.nameSong}>{name}</div>
+//             <div className={styles.nameArtist}>{artist}</div>
+//             <div className={styles.nameAlbum}>{album}</div>
+//           </div>
+//           {/*Buttons add and play*/}
+//           <div className={styles.iconsContainer}>
+//             <PlayIcon className={styles.icon} />
+//             <AddIcon className={styles.icon} />
+//           </div>
+//         </div>
+//         <hr/>
+//     </div>
+//   </>
+//   );
+// }
+
+// export default Track;
